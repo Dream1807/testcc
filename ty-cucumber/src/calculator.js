9065a9f20af7b1de9059379b26ffdc5104d8c7b7
Meng Liu <meng@canva.com>
2020-05-29 18:15:20 +0800

    11

---
+++ ty-cucumber/src/calculator.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Calculator = void 0;
+var Calculator = /** @class */ (function () {
+    function Calculator() {
+    }
+    Calculator.prototype.divide = function (n1, n2) {
+        return n1 / n2;
+    };
+    return Calculator;
+}());
+exports.Calculator = Calculator;
+//# sourceMappingURL=calculator.js.map
